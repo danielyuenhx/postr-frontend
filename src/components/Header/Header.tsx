807be89dec6368f8f03b0416fd9ac274da0e7dff
danielyuenhx <yuendaniel33@gmail.com>
2022-08-28 23:13:02 +0800

    added logo to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import logo from '../../images/postr-logo.png';
+// #1390F4
 
 const Header = () => {
-    return (
-        <header className="fixed top-0 left-0 w-full h-20 flex justify-between items-center px-[10%] shadow-md">
-            <h1 className="font-bold">postr</h1>
-        </header>
-    )
-}
+	return (
+		<header className="fixed top-0 left-0 w-full h-20 flex justify-between items-center px-[10%] shadow-md">
+			{/* <h1 className="font-bold">postr</h1> */}
+			<img src={logo} alt="logo" className="w-28" />
+		</header>
+	);
+};
 
 export default Header;
