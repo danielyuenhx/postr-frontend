f16a21e013d37291925b163a1891ed59aa6864be
danielyuenhx <yuendaniel33@gmail.com>
2022-08-28 22:48:00 +0800

    removed unused files, added typescript config and added header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="fixed top-0 left-0 w-full h-20 flex justify-between items-center px-[10%] shadow-md">
+            <h1 className="font-bold">postr</h1>
+        </header>
+    )
+}
+
+export default Header;
