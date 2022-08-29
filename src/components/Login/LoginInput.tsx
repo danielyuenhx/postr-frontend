cc85dfc1728d6dd5cd8e61a99a22588fb189ab1c
danielyuenhx <yuendaniel33@gmail.com>
2022-08-30 04:50:38 +0800

    added LoginInput component

--- src/components/Login/LoginInput.tsx
+++ src/components/Login/LoginInput.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import styles from './LoginInput.module.css';
 
 const LoginInput = () => {
+	const [value, setValue] = useState('');
+
+	const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(event.target.value);
+	};
+
 	return (
-        <label></label>
-    )
+		<div className={styles.container}>
+			<label className={value && styles.filled}>Label</label>
+			<input type="text" value={value} onChange={onChangeHandler} />
+		</div>
+	);
 };
 
 export default LoginInput;
