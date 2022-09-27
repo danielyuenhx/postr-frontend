75827c66d95e66b4a0511ee9b7034d12c9d11c13
danielyuenhx <yuendaniel33@gmail.com>
2022-09-27 18:14:56 +0800

    added animations for liking a post

--- src/components/Home/icons/Like.tsx
+++ src/components/Home/icons/Like.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
-import { debounce } from 'lodash';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 import styles from './Like.module.css';
 
@@ -8,11 +8,8 @@ const Like = (props: {
 	isLikedByUser: boolean;
 	onClick: () => void;
 }) => {
+	const [initialState, setInitialState] = useState(true);
 	const [isLiked, setIsLiked] = useState(props.isLikedByUser);
-
-	// const debouncedClick = useCallback(debounce(() => {
-	//     props.onClick;
-	//   }, INTERVAL, {leading: true, trailing: false, maxWait: INTERVAL}), []);
 
 	const likeHandler = async (event: React.MouseEvent<SVGSVGElement>) => {
 		await props.onClick();
@@ -21,25 +18,23 @@ const Like = (props: {
 		} else {
 			setIsLiked(true);
 		}
+        setInitialState(false)
 	};
 
 	return (
-		<svg
+		<motion.svg
+			whileHover={{ scale: 1.125 }}
+			whileTap={{ scale: 0.9 }}
 			className={`${styles.heart} ${
 				isLiked ? styles.like : styles.unlike
-				// isLiked === 'first'
-				// 	? ''
-				// 	: isLiked === 'liked'
-				// 	? styles.like
-				// 	: styles.unlike
-			}`}
+			} ${initialState ? styles.pageload : ''}`}
 			viewBox="0 0 20 20"
 			xmlns="http://www.w3.org/2000/svg"
 			stroke="currentColor"
 			onClick={props.isLoggedIn ? likeHandler : () => {}}
 		>
 			<path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
-		</svg>
+		</motion.svg>
 	);
 };
 
