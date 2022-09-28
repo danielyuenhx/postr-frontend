fd9a4ef7e1e160a95f9bdb2f65aacc208b1c880d
danielyuenhx <yuendaniel33@gmail.com>
2022-09-28 20:15:05 +0800

    added profile section in profile page

--- src/components/ProfilePage/Profile/Profile.tsx
+++ src/components/ProfilePage/Profile/Profile.tsx
@@ -3,6 +3,10 @@ import LetteredAvatar from 'react-lettered-avatar';
 import moment from 'moment';
 
 import styles from './Profile.module.css';
+import Like from '../../Home/icons/Like';
+import Heart from '../icons/Heart';
+import Write from '../icons/Write';
+import Birthday from '../icons/Birthday';
 
 type User = {
 	username: string;
@@ -11,14 +15,39 @@ type User = {
 	totalLikes: number;
 };
 
-const Profile = (props: {user: User}) => {
+const Profile = (props: { user: User }) => {
 	return (
 		<div className={styles.container}>
-			<LetteredAvatar name={props.user.username} size={60} />
-			<p>{props.user.username}</p>
-			Joined {moment(props.user.createdAt).fromNow()}
-			<p>Total posts: {props.user.totalPosts}</p>
-			<p>Total likes: {props.user.totalLikes}</p>
+			<div className={styles.avatar}>
+				<LetteredAvatar name={props.user.username} size={60} />
+			</div>
+			<h2>{props.user.username}</h2>
+			<p className={styles.fromNow}>
+				Joined {moment(props.user.createdAt).fromNow()}
+			</p>
+			<div className={styles.stats}>
+				<div className={styles.stat}>
+					<p>postr day</p>
+					<div>
+						<Birthday />
+						<span>{moment(props.user.createdAt).format('MMMM DD, YYYY')}</span>
+					</div>
+				</div>
+				<div className={styles.smallStats}>
+					<div className={styles.stat}>
+						<p>Likes</p>
+						<div>
+							<Heart /> <span>{props.user.totalLikes}</span>
+						</div>
+					</div>
+					<div className={styles.stat}>
+						<p>Posts</p>
+						<div>
+							<Write /> <span>{props.user.totalPosts}</span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/components/ProfilePage/icons/Heart.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Heart = () => {
+	return (
+		<svg
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+			stroke="#1083FD"
+			fill="none"
+		>
+			<path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+		</svg>
+	);
+};
+
+export default Heart;
