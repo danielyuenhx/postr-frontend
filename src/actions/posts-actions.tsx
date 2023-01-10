b6da05ffccac5e7472bac15afe8161f7f4b6c2ef
Daniel Yuen <yuendaniel33@gmail.com>
2023-01-10 15:29:12 +0800

    handle excessive requests

--- src/actions/posts-actions.tsx
+++ src/actions/posts-actions.tsx
@@ -6,79 +6,84 @@ import { postsActions } from '../store/posts-slice';
 import { AppDispatch } from '../store/index';
 
 type Post = {
-	user: string;
-	title: string;
-	content: string;
-	tags: string;
-	selectedFile: string;
-	createdAt: Date;
+  user: string;
+  title: string;
+  content: string;
+  tags: string;
+  selectedFile: string;
+  createdAt: Date;
 };
 
 type NewPost = {
-	user: string;
-	title: string;
-	content: string;
-	tags: string;
-	selectedFile: string;
-	picture: string;
+  user: string;
+  title: string;
+  content: string;
+  tags: string;
+  selectedFile: string;
+  picture: string;
 };
 
 type Error = { message: string };
 
 export const getPosts = () => async (dispatch: AppDispatch) => {
-	try {
-		const { data } = await api.getPosts();
+  try {
+    const { data } = await api.getPosts();
 
-		dispatch(postsActions.getPosts(data));
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			return (error.response?.data as Error).message;
-		} else {
-			return 'An unexpected error occurred';
-		}
-	}
+    dispatch(postsActions.getPosts(data));
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return (error.response?.data as Error).message;
+    } else {
+      return 'An unexpected error occurred';
+    }
+  }
 };
 
 export const createPost =
-	(postData: NewPost) => async (dispatch: AppDispatch) => {
-		try {
-			const { data } = await api.createPost(postData);
-			const updatedData = {...data, picture: postData.picture}
+  (postData: NewPost) => async (dispatch: AppDispatch) => {
+    try {
+      const { data } = await api.createPost(postData);
 
-			dispatch(postsActions.createPost(updatedData));
-		} catch (error) {
-			if (axios.isAxiosError(error) && error.response) {
-				return (error.response?.data as Error).message;
-			} else {
-				return 'An unexpected error occurred';
-			}
-		}
-	};
+      const updatedData = { ...data, picture: postData.picture };
+
+      dispatch(postsActions.createPost(updatedData));
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        if (error.response.status === 429) {
+          return error.response?.data;
+        } else {
+          return (error.response?.data as Error).message;
+        }
+      } else {
+        return 'An unexpected error occurred';
+      }
+    }
+  };
 
 export const deletePost = (id: string) => async (dispatch: AppDispatch) => {
-	try {
-		await api.deletePost(id);
+  try {
+    await api.deletePost(id);
 
-		dispatch(postsActions.deletePost(id));
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			return (error.response?.data as Error).message;
-		} else {
-			return 'An unexpected error occurred';
-		}
-	}
+    dispatch(postsActions.deletePost(id));
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return (error.response?.data as Error).message;
+    } else {
+      return 'An unexpected error occurred';
+    }
+  }
 };
 
 export const likePost = (id: string) => async (dispatch: AppDispatch) => {
-	try {
-		const { data } = await api.likePost(id);
+  try {
+    const { data } = await api.likePost(id);
 
-		dispatch(postsActions.updatePost(data));
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			return (error.response?.data as Error).message;
-		} else {
-			return 'An unexpected error occurred';
-		}
-	}
+    dispatch(postsActions.updatePost(data));
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return (error.response?.data as Error).message;
+    } else {
+      return 'An unexpected error occurred';
+    }
+  }
 };
